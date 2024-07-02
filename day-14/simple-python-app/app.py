a20b2bdb02cc2d269a6f9b0d50a5588bd85f7f4c
Kondal <61467923+kondal123@users.noreply.github.com>
2024-07-02 10:26:03 +0530

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello people, This is my first CI/CD project!!!!'
 
 if __name__ == '__main__':
     app.run()
